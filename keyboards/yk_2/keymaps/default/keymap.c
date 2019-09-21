a61d1357fdf927af6f5eba296ec3ba5c453cc9b1
yoyo <yoyo@hagephone.jp>
2019-09-21 21:18:45 +0900

    キーマップ設定ミス

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -15,13 +15,13 @@
  */
 #include QMK_KEYBOARD_H
 
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { \
-    [0] = LAYOUT( /* Base */ \
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, \
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, \
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, \
-        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, \
-                          KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I \
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { 
+    [0] = LAYOUT( /* Base */ 
+        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
+        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
+        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
+        KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
+                          KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I 
         )
 };
 
