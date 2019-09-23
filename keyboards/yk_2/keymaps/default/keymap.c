48bea8330f5fe860314978bac42eea73e3c09622
yoyo <yoyo@hagephone.jp>
2019-09-23 22:54:16 +0900

    存在しないキーを指定していたバグを修正

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -17,7 +17,7 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { 
     [0] = LAYOUT( /* Base */ 
-        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6  ,  KC_7, KC_8, KC_9, KC_0, KC_-, KC_=, KC_Z,
+        KC_1, KC_2, KC_3, KC_4, KC_5, KC_6  ,  KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_AT, KC_Z,
         KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
         KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
         KC_A, KC_B, KC_C, KC_D, KC_E, KC_F  ,  KC_G, KC_H, KC_I, KC_J, KC_L, KC_M, KC_N,
