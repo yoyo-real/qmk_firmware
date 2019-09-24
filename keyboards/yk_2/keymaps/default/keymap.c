84550064478f372e4f341c190c036d6465888e27
yoyo <yoyo@hagephone.jp>
2019-09-25 01:47:14 +0900

    BottomにCtrl+Rを追加

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -38,7 +38,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         ),
     [_CTRL_NUMPAD] = LAYOUT(
         _______, _______, _______, _______, _______, _______  ,  KC_TAB , KC_PSLS, KC_PAST, KC_BSPC, _______, _______, _______,
-        _______, _______, LCTL(KC_W), _______, _______, LCTL(KC_T)  ,  KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______, _______, _______,
+        _______, _______, LCTL(KC_W), _______, LCTL(KC_R), LCTL(KC_T)  ,  KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______, _______, _______,
         KC_LSFT, LCTL(KC_A), LCTL(KC_S), _______, LCTL(KC_TAB), _______  ,  KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______, _______, _______,
         _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______  ,  KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______, _______, _______,
                                    _______, _______, _______  ,  KC_P0  , _______, KC_PDOT 
