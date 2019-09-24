dc995e23d2f7e011f34fc02e2c88fafe8744afed
yoyo <yoyo@hagephone.jp>
2019-09-25 01:07:25 +0900

    スペース長押しで一部ショートカットを使えるように設定

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -18,7 +18,7 @@
 enum layer_number {
     _BASE = 0,
     _RAISE,
-    _NUMPAD
+    _CTRL_NUMPAD
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = { 
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T ,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,
         KC_LCTL,KC_A,  KC_S,  KC_D,  KC_F,  KC_G ,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_ENT,
         KC_GRV, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B ,  KC_N,  KC_M,  KC_COMM,KC_DOT,KC_SLSH, KC_BSLS,  KC_BSPC,
-                     KC_LALT,  KC_LSFT,  LT(_NUMPAD, KC_SPC)  ,  MO(_RAISE),  KC_RSFT,  KC_RWIN 
+                     KC_LALT,  KC_LSFT,  LT(_CTRL_NUMPAD, KC_SPC)  ,  MO(_RAISE),  KC_RSFT,  KC_RWIN 
         ),
     [_RAISE] = LAYOUT(
         _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5    ,  KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
@@ -36,11 +36,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, _______, KC_PGUP  ,  _______, _______, _______, _______, _______, _______, KC_DEL,
                                    _______, _______, LALT(KC_GRV),  _______, _______, _______ 
         ),
-    [_NUMPAD] = LAYOUT(
+    [_CTRL_NUMPAD] = LAYOUT(
         _______, _______, _______, _______, _______, _______  ,  KC_TAB , KC_PSLS, KC_PAST, KC_BSPC, _______, _______, _______,
         _______, _______, _______, _______, _______, _______  ,  KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______  ,  KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______, _______, _______,
-        _______, _______, _______, _______, _______, _______  ,  KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______, _______, _______,
+        _______, LALT(LSFT(KC_TAB)), LALT(KC_TAB), LCTL(LSFT(KC_TAB)), LCTL(KC_TAB), _______  ,  KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______, _______, _______,
+        _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______  ,  KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______, _______, _______,
                                    _______, _______, _______  ,  KC_P0  , _______, KC_PDOT 
         ),
 };
