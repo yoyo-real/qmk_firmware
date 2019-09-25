3656d5b19c74008da148fe3819b843f862ea0daf
yoshitaka-kantou <yoshitaka-kantou@tmc.co.jp>
2019-09-25 09:48:51 +0900

    シフトとテンキーを調整

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -26,7 +26,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC, KC_1,  KC_2,  KC_3,  KC_4,  KC_5 ,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,
         KC_TAB, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T ,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,
         KC_LCTL,KC_A,  KC_S,  KC_D,  KC_F,  KC_G ,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_ENT,
-        KC_GRV, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B ,  KC_N,  KC_M,  KC_COMM,KC_DOT,KC_SLSH, KC_BSLS,  KC_BSPC,
+        KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B ,  KC_N,  KC_M,  KC_COMM,KC_DOT,KC_SLSH, KC_BSLS,  KC_BSPC,
                      KC_LALT, MO(_CTRL_NUMPAD),  KC_SPC  ,  MO(_RAISE),  KC_RSFT,  KC_RWIN 
         ),
     [_RAISE] = LAYOUT(
@@ -37,10 +37,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                    _______, _______, LALT(KC_GRV),  _______, _______, _______ 
         ),
     [_CTRL_NUMPAD] = LAYOUT(
-        _______, _______, _______, _______, _______, _______  ,  KC_TAB , KC_PSLS, KC_PAST, KC_BSPC, _______, _______, _______,
+        KC_GRV, _______, _______, _______, _______, _______  ,  KC_TAB , KC_PSLS, KC_PAST, KC_BSPC, _______, _______, _______,
         _______, _______, LCTL(KC_W), _______, LCTL(KC_R), LCTL(KC_T)  ,  KC_P7  , KC_P8  , KC_P9  , KC_PMNS, _______, _______, _______,
-        KC_LSFT, LCTL(KC_A), LCTL(KC_S), _______, LCTL(KC_TAB), _______  ,  KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______, _______, _______,
-        _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______  ,  KC_P1  , KC_P2  , KC_P3  , KC_PENT, _______, _______, _______,
+        _______, LCTL(KC_A), LCTL(KC_S), _______, LCTL(KC_TAB), _______  ,  KC_P4  , KC_P5  , KC_P6  , KC_PPLS, _______, KC_UP, _______,
+        _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______  ,  KC_P1  , KC_P2  , KC_P3  , KC_PENT, KC_LEFT, KC_DOWN, KC_RIGHT,
                                    _______, _______, _______  ,  KC_P0  , _______, KC_PDOT 
         ),
 };
