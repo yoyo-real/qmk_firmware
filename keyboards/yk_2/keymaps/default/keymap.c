754b2c34508b02c631da57e815800ae982007213
yoshitaka-kantou <yoshitaka-kantou@tmc.co.jp>
2019-09-26 10:35:18 +0900

    Modキー周りを色々と変更

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -18,7 +18,6 @@
 enum layer_number {
     _BASE = 0,
     _RAISE,
-    _LOWER,
     _NUMPAD
 };
 
@@ -26,9 +25,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT( /* Base */ 
         KC_ESC, KC_1,  KC_2,  KC_3,  KC_4,  KC_5 ,  KC_6,  KC_7,  KC_8,  KC_9,  KC_0,  KC_MINS,  KC_EQL,
         KC_TAB, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T ,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,
-        KC_LCTL,KC_A,  KC_S,  KC_D,  KC_F,  KC_G ,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_NO,
+        CTL_T(KC_TAB), KC_A,  KC_S,  KC_D, SFT_T(KC_F),  KC_G ,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_NO,
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B ,  KC_N,  KC_M,  KC_COMM,KC_DOT,KC_SLSH, KC_BSLS,  KC_BSPC,
-                     KC_LWIN, KC_LALT, LT(_LOWER, KC_SPC)  ,  LT(_RAISE, KC_ENT),  KC_RSFT,  TT(_NUMPAD) 
+                     KC_LWIN, KC_LALT, CTL_T(KC_SPC)  ,  LT(_RAISE, KC_ENT),  KC_RSFT,  TT(_NUMPAD) 
         ),
     [_RAISE] = LAYOUT(
         _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5    ,  KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
@@ -36,13 +35,6 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, KC_PGDN, _______  ,  KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, KC_GRV, _______,
         _______, _______, _______, _______, _______, KC_PGUP  ,  _______, _______, _______, _______, _______, _______, KC_DEL,
                                    _______, _______, LALT(KC_GRV),  _______, _______, _______ 
-        ),
-    [_LOWER] = LAYOUT(
-        _______, _______, _______, _______, _______, _______  ,  _______, _______, _______, _______, _______, _______, _______,
-        _______, _______, LCTL(KC_W), _______, LCTL(KC_R), LCTL(KC_T)  ,  _______, _______, _______, _______, _______, _______, _______,
-        _______, LCTL(KC_A), LCTL(KC_S), _______, LCTL(KC_TAB), _______  ,  _______, _______, _______, _______, _______, _______, _______,
-        _______, LCTL(KC_Z), LCTL(KC_X), LCTL(KC_C), LCTL(KC_V), _______  ,  _______, _______, _______, _______, _______, _______, _______,
-                                   _______, _______, _______  ,  _______, _______, _______ 
         ),
     [_NUMPAD] = LAYOUT(
         _______, _______, _______, _______, _______, _______  ,  KC_TAB , KC_PSLS, KC_PAST, KC_BSPC, KC_DEL, _______, _______,
