173663ff247a8a2dba4c076d3f28deca25b7ec3c
yoshitaka-kantou <yoshitaka-kantou@tmc.co.jp>
2019-09-26 18:03:54 +0900

    LCtrl周りの割当を修正、逆タブ送りがやりやすいように

--- keyboards/yk_2/keymaps/default/keymap.c
+++ keyboards/yk_2/keymaps/default/keymap.c
@@ -18,6 +18,7 @@
 enum layer_number {
     _BASE = 0,
     _RAISE,
+    _LOWER,
     _NUMPAD
 };
 
@@ -27,7 +28,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB, KC_Q,  KC_W,  KC_E,  KC_R,  KC_T ,  KC_Y,  KC_U,  KC_I,  KC_O,  KC_P,  KC_LBRC,  KC_RBRC,
         CTL_T(KC_TAB), KC_A,  KC_S,  KC_D, KC_F,  KC_G ,  KC_H,  KC_J,  KC_K,  KC_L,  KC_SCLN,  KC_QUOT,  KC_NO,
         KC_LSFT, KC_Z,  KC_X,  KC_C,  KC_V,  KC_B ,  KC_N,  KC_M,  KC_COMM,KC_DOT,KC_SLSH, KC_BSLS,  KC_BSPC,
-                     KC_LWIN, KC_LALT, CTL_T(KC_SPC)  ,  LT(_RAISE, KC_ENT),  KC_RSFT,  TT(_NUMPAD) 
+                     KC_LWIN, KC_LALT, LT(_LOWER, KC_SPC)  ,  LT(_RAISE, KC_ENT),  KC_RSFT,  TT(_NUMPAD) 
         ),
     [_RAISE] = LAYOUT(
         _______, KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5    ,  KC_F6  , KC_F7  , KC_F8  , KC_F9  , KC_F10 , KC_F11 , KC_F12 ,
@@ -35,6 +36,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______, _______, _______, _______, KC_PGDN, _______  ,  KC_LEFT, KC_DOWN, KC_UP  , KC_RIGHT, _______, KC_GRV, _______,
         _______, _______, _______, _______, _______, KC_PGUP  ,  _______, _______, _______, _______, _______, _______, KC_DEL,
                                    _______, _______, LALT(KC_GRV),  _______, _______, _______ 
+        ),
+    [_LOWER] = LAYOUT( /* With ctrl */
+        _______, _______, _______, _______, _______, _______  ,  _______, _______, _______, _______, _______, _______, _______,
+        LSFT(KC_TAB), _______, _______, _______, _______, _______  ,  _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______  ,  _______, _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______  ,  _______, _______, _______, _______, _______, _______, _______,
+                                   _______, _______, _______,  _______, _______, _______ 
         ),
     [_NUMPAD] = LAYOUT(
         _______, _______, _______, _______, _______, _______  ,  KC_TAB , KC_PSLS, KC_PAST, KC_BSPC, KC_DEL, _______, _______,
@@ -52,4 +60,16 @@ bool is_keyboard_master(void) {
 #else
     return readPin(SPLIT_HAND_PIN);
 #endif
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+    switch (get_highest_layer(state)) {
+    case _LOWER:
+        add_mods(MOD_BIT(KC_LCTL));
+        break;
+    default:
+        del_mods(MOD_BIT(KC_LCTL));
+        break;
+    }
+  return state;
 }
