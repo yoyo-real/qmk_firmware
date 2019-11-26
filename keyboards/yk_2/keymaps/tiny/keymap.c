cd4491dd9bacd22c7c2095b9f480ae5ca46ec768
yoshitaka-kantou <yoshitaka-kantou@tmc.co.jp>
2019-11-26 11:26:02 +0900

    田をRALTに変更

--- keyboards/yk_2/keymaps/tiny/keymap.c
+++ keyboards/yk_2/keymaps/tiny/keymap.c
@@ -31,7 +31,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_ESC        , KC_Q   , KC_W   , KC_E   , KC_R   , KC_T     ,  KC_Y   , KC_U   , KC_I   , KC_O   , KC_P   , KC_MINS, XXXXXXX,
         CTL_T(KC_TAB) , KC_A   , KC_S   , KC_D   , KC_F   , KC_G     ,  KC_H   , KC_J   , KC_K   , KC_L   , KC_SCLN, KC_QUOT, XXXXXXX,
         SFT_T(KC_BSPC), KC_Z   , KC_X   , KC_C   , KC_V   , KC_B     ,  KC_N   , KC_M   , KC_COMM, KC_DOT , KC_SLSH, KC_BSLS, XXXXXXX,
-                                KC_LALT, KC_LCTL, LT(_LOWER,KC_SPC)  ,  LT(_RAISE, KC_ENT), SFT_IME,  KC_RWIN
+                                KC_LALT, KC_LCTL, LT(_LOWER,KC_SPC)  ,  LT(_RAISE, KC_ENT), SFT_IME, KC_RALT
         ),
     [_RAISE] = LAYOUT(
         _______, _______, _______, _______, _______, _______  ,  _______, _______, _______, _______, _______, _______, _______,
