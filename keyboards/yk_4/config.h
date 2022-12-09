0965097d9cebe724bc771b074fb20690347e3f78
yoyo <yoyo@hagephone.jp>
2022-12-10 04:10:58 +0900

    enable PERMISSIVE_HOLD

--- keyboards/yk_4/config.h
+++ keyboards/yk_4/config.h
@@ -204,3 +204,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 /* Bootmagic Lite key configuration */
 //#define BOOTMAGIC_LITE_ROW 0
 //#define BOOTMAGIC_LITE_COLUMN 0
+
+#define PERMISSIVE_HOLD
